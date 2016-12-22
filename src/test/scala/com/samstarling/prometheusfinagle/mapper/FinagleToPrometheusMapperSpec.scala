81e6c2b3eefcc88ba8456bd73ad780958d324761
Sam Starling <mail@samstarling.co.uk>
2016-12-22 17:25:42 +0000

    Start adding unit tests for FinagleToPrometheusMapper

--- src/test/scala/com/samstarling/prometheusfinagle/mapper/FinagleToPrometheusMapperSpec.scala
+++ src/test/scala/com/samstarling/prometheusfinagle/mapper/FinagleToPrometheusMapperSpec.scala
@@ -4,10 +4,28 @@ import com.samstarling.prometheusfinagle.UnitTest
 import com.twitter.common.metrics.Metrics
 import org.specs2.specification.Scope
 
+import scala.collection.JavaConverters._
+
 class FinagleToPrometheusMapperSpec extends UnitTest {
 
   trait Context extends Scope {
     val metrics = Metrics.createDetached
     val mapper = new FinagleToPrometheusMapper(metrics)
   }
+
+  "#metricFamilySamples" >> {
+
+    "counters" >> {
+      trait CounterContext extends Context {
+        metrics.registerCounter("number_of_bears")
+        metrics.registerCounter("number_of_dogs")
+        metrics.registerCounter("number_of_cats")
+      }
+
+      "returns a metric for each counter" in new CounterContext {
+        mapper.metricFamilySamples must have size(3)
+      }
+    }
+  }
 }
+
