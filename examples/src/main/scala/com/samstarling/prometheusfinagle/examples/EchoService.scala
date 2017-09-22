795a20e95de4120f9c4954d255a6dffb1d638fd1
Sam Starling <sam@soundcloud.com>
2017-09-22 16:45:46 +0200

    Implement a basic PrometheusStatsReceiver, tidy up some examples

--- examples/src/main/scala/com/samstarling/prometheusfinagle/examples/EchoService.scala
+++ examples/src/main/scala/com/samstarling/prometheusfinagle/examples/EchoService.scala
@@ -4,11 +4,10 @@ import com.twitter.finagle.Service
 import com.twitter.finagle.http.{Request, Response, Status}
 import com.twitter.util.Future
 
-case class EchoService(message: String) extends Service[Request, Response] {
+class EchoService extends Service[Request, Response] {
   def apply(req: Request): Future[Response] = {
     val rep = Response(req.version, Status.Ok)
-    rep.setContentString(
-      s"$message, the ID parameter was: ${req.params.get("id")}")
+    rep.setContentString(s"Parameters: ${req.params.mkString(", ")}")
     Future(rep)
   }
 }
--- src/main/scala/com/samstarling/prometheusfinagle/filter/HttpLatencyMonitoringFilter.scala
+++ src/main/scala/com/samstarling/prometheusfinagle/filter/HttpLatencyMonitoringFilter.scala
@@ -7,9 +7,8 @@ import com.twitter.util.{Future, Stopwatch}
 
 class HttpLatencyMonitoringFilter(telemetry: Telemetry,
                                   buckets: Seq[Double],
-                                  labeller: RequestLabeller =
-                                    new HttpRequestLabeller)
-    extends SimpleFilter[Request, Response] {
+                                  labeller: HttpServiceLabeller = new HttpServiceLabeller)
+  extends SimpleFilter[Request, Response] {
 
   private val histogram = telemetry.histogram(
     name = "incoming_http_request_latency_seconds",
@@ -18,12 +17,11 @@ class HttpLatencyMonitoringFilter(telemetry: Telemetry,
     buckets = buckets
   )
 
-  override def apply(request: Request,
-                     service: Service[Request, Response]): Future[Response] = {
+  override def apply(request: Request, service: Service[Request, Response]): Future[Response] = {
     val stopwatch = Stopwatch.start()
     service(request) onSuccess { response =>
-      val labels = labeller.labelsFor(request, response)
-      histogram.labels(labels: _*).observe(stopwatch().inMilliseconds / 1000.0)
+      histogram.labels(labeller.labelsFor(request, response): _*)
+        .observe(stopwatch().inMilliseconds / 1000.0)
     }
   }
 }
