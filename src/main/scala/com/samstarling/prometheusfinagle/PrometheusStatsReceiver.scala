9fb8e58ffcfa82d341dcc7e84472c9a01c942a3c
Alessandro Vermeulen <github@spockz.nl>
2017-09-24 14:42:24 +0200

    Map all (known) finagle metrics, patterns can be made smaller probably with the good default.

--- src/main/scala/com/samstarling/prometheusfinagle/PrometheusStatsReceiver.scala
+++ src/main/scala/com/samstarling/prometheusfinagle/PrometheusStatsReceiver.scala
@@ -7,35 +7,43 @@ import scala.collection.concurrent.TrieMap
 
 class PrometheusStatsReceiver(registry: CollectorRegistry, namespace: String) extends StatsReceiver {
 
-  private val counters = TrieMap.empty[Seq[String], PCounter]
-  private val histograms = TrieMap.empty[Seq[String], PHistogram]
-  private val gauges = TrieMap.empty[Seq[String], PGauge]
+  private val counters = TrieMap.empty[String, PCounter]
+  private val histograms = TrieMap.empty[String, PHistogram]
+  private val gauges = TrieMap.empty[String, PGauge]
 
   private val helpMessage = "Help is not currently available"
 
   override def repr: AnyRef = this
 
-  override def counter(verbosity: Verbosity, name: String*): Counter = new Counter {
-    override def incr(delta: Long): Unit = {
-      val (_, labelValues) = extractLabelValues(name)
-      counters.getOrElseUpdate(name, newCounter(name)).labels(labelValues: _*).inc(delta)
+  override def counter(verbosity: Verbosity, name: String*): Counter = {
+    val (metricName, labels) = extractLabels(name)
+
+    new Counter {
+      override def incr(delta: Long): Unit = {
+        counters.getOrElseUpdate(metricName, newCounter(metricName, labels.keys.toSeq)).labels(labels.values.toSeq: _*).inc(delta)
+      }
     }
   }
 
-  override def stat(verbosity: Verbosity, name: String*): Stat = new Stat {
-    override def add(value: Float): Unit = {
-      histograms.getOrElseUpdate(name, newHistogram(name)).observe(value)
+  override def stat(verbosity: Verbosity, name: String*): Stat = {
+    val (metricName, labels) = extractLabels(name)
+    new Stat {
+      override def add(value: Float): Unit = {
+        histograms.getOrElseUpdate(metricName, newHistogram(metricName, labels.keys.toSeq)).labels(labels.values.toSeq: _*).observe(value)
+      }
     }
   }
 
-  override def addGauge(verbosity: Verbosity, name: String*)(f: => Float): Gauge = new Gauge {
-    gauges.getOrElseUpdate(name, newGauge(name)).set(f)
-    override def remove(): Unit = gauges.remove(name)
+  override def addGauge(verbosity: Verbosity, name: String*)(f: => Float): Gauge = {
+    val (metricName, labels) = extractLabels(name)
+    gauges.getOrElseUpdate(metricName, newGauge(metricName, labels.keys.toSeq)).labels(labels.values.toSeq: _*).set(f)
+
+    new Gauge {
+      override def remove(): Unit = gauges.remove(metricName)
+    }
   }
 
-  private def newCounter(name: Seq[String]): PCounter = {
-    val (metricName, labelNames) =
-      extractLabelNames(name)
+  private def newCounter(metricName: String, labelNames: Seq[String]): PCounter = {
     PCounter.build()
       .namespace(namespace)
       .name(metricName)
@@ -44,10 +52,7 @@ class PrometheusStatsReceiver(registry: CollectorRegistry, namespace: String) ex
       .register(registry)
   }
 
-  private def newHistogram(name: Seq[String]): PHistogram = {
-    val (metricName, labelNames) =
-      extractLabelNames(name)
-
+  private def newHistogram(metricName: String, labelNames: Seq[String]): PHistogram = {
     PHistogram.build()
       .namespace(namespace)
       .name(metricName)
@@ -57,10 +62,7 @@ class PrometheusStatsReceiver(registry: CollectorRegistry, namespace: String) ex
       .register(registry)
   }
 
-  private def newGauge(name: Seq[String]): PGauge = {
-    val (metricName, labelNames) =
-      extractLabelNames(name)
-
+  private def newGauge(metricName: String, labelNames: Seq[String]): PGauge = {
     PGauge.build()
       .namespace(namespace)
       .name(metricName)
@@ -73,16 +75,65 @@ class PrometheusStatsReceiver(registry: CollectorRegistry, namespace: String) ex
     name.map(_.replaceAll("[^\\w]", "_")).mkString("__")
   }
 
-  protected def extractLabelNames(name: Seq[String]): (String, Seq[String]) = name match {
-    case Seq(label, "requests") =>
-      ("requests", Seq("serviceName"))
-    case default => (sanitizeName(default), Seq.empty[String])
+  def metricPattern: DefaultMetricPatterns.Pattern = DefaultMetricPatterns.All
+
+  protected def extractLabels(name: Seq[String]): (String, Map[String, String]) = {
+    metricPattern.applyOrElse(name, (x: Seq[String]) => sanitizeName(x) -> Map.empty)
   }
 
-  protected def extractLabelValues(name: Seq[String]): (String, Seq[String]) = name match {
-    case Seq(label, "requests") =>
-      ("requests", Seq(label))
-    case default => (sanitizeName(default), Seq.empty[String])
+
+}
+
+object DefaultMetricPatterns {
+  type Pattern = PartialFunction[Seq[String], (String, Map[String, String])]
+
+  val prometheusLabelForLabel = "serviceName"
+
+  val Core: Pattern = {
+    case Seq(label, metric) =>
+      (metric, Map(prometheusLabelForLabel -> label))
+    case Seq(label, "request_payload_bytes") =>
+      ("request_payload_bytes", Map(prometheusLabelForLabel -> label))
+    case Seq(label, "response_payload_bytes") =>
+      ("response_payload_bytes", Map(prometheusLabelForLabel -> label))
+    case Seq(label, "retries", metric @ ("request_limit" | "budget_exhausted" | "requeues_per_request" | "budget" | "requeues" | "cannot_retry" | "not_open")) =>
+      (s"retries_$metric", Map(prometheusLabelForLabel -> label))
+    case Seq(label, "nack_admission_control", metric @ ("dropped_requests")) =>
+      (s"nack_admission_control_$metric", Map(prometheusLabelForLabel -> label))
+    case Seq(label, "failfast", metric @ ("unhealthy_for_ms" | "marked_dead" | "marked_available" | "unhealthy_num_tries")) =>
+      (s"failfast_$metric", Map(prometheusLabelForLabel -> label))
+    case Seq(label, "dtab", metric @ ("size")) =>
+      (s"dtab_$metric", Map(prometheusLabelForLabel -> label))
+    case Seq(label, "dispatcher", "serial", "queue_size") =>
+      ("dispatcher_serial_queue_size", Map(prometheusLabelForLabel -> label))
+    case Seq(label, "service_creation", "service_acquisition_latency_ms") =>
+      ("service_creation_service_acquisition_latency_ms", Map(prometheusLabelForLabel -> label))
   }
 
+  val FailureAccrual: Pattern = {
+    case Seq(label, "failure_accrual", metric@("removals" | "revivals" | "probes" | "removed_for_ms")) =>
+      (s"loadbalancer_$metric", Map(prometheusLabelForLabel -> label))
+  }
+
+  val LoadBalancer: Pattern = {
+    case Seq(label, "loadbalancer", metric@("busy"|"available"|"size"|"adds"|"meanweight"|"closed"|"load"|"rebuilds"|"updates"|"max_effort_exhausted"|"removes")) =>
+      (s"loadbalancer_$metric", Map(prometheusLabelForLabel -> label))
+    case Seq(label, "loadbalancer", "algorithm", algorithm) =>
+      (s"loadbalancer_algorithm", Map(prometheusLabelForLabel -> label, "algorithm" -> algorithm))
+  }
+
+  val Http: Pattern = {
+    case Seq(label, "http", "time", resultCode) =>
+      ("http_request_duration", Map(prometheusLabelForLabel -> label, "resultCode" -> resultCode))
+    case Seq(label, "http", "status", resultCode) =>
+      ("http_request_classification", Map(prometheusLabelForLabel -> label, "resultCode" -> resultCode))
+    case Seq(label, "http", "response_size") =>
+      ("http_response_size", Map(prometheusLabelForLabel -> label))
+  }
+
+  val All: Pattern =
+    Core
+      .orElse(LoadBalancer)
+      .orElse(FailureAccrual)
+      .orElse(Http)
 }
